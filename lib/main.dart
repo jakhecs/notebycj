d6e22fb75be4a91df809b4865beac16e8b9bed79
jakhecs <jakhecs@gmail.com>
2025-05-28 14:28:51 +0000

    Etape 13: préparation de l'interface d'ajout de note

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 import 'package:notesbycj/constants/routes.dart';
 import 'package:notesbycj/views/login_view.dart';
-import 'package:notesbycj/views/notes_view.dart';
+import 'package:notesbycj/views/notes/notes_view.dart';
 import 'package:notesbycj/views/register_view.dart';
 import 'package:notesbycj/views/verify_email_view.dart';
 import 'package:notesbycj/services/auth/auth_service.dart';
+import 'package:notesbycj/views/notes/new_note_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +21,7 @@ void main() {
       notesRoute: (context) => const NotesView(),
       registerRoute: (context) => const RegisterView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
+      newNoteRoute: (context) => const NewNoteView(),
     },
   ));
 }
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -100,7 +100,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == "test@test.com") throw UserNotFoundAuthException();
     if (password == "testuser") throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: '');
+    const user = AuthUser(isEmailVerified: false, email: 'test@test.com');
     _user = user;
     return Future.value(user);
   }
@@ -118,7 +118,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotLoggedInAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: '');
+    const newUser = AuthUser(isEmailVerified: true, email: 'test@test.com');
     _user = newUser;
   }
 }
