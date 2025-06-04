6287d5f37cd19a273ce6d53e370ceb96744a11d1
jakhecs <jakhecs@gmail.com>
2025-06-04 13:53:19 +0000

    Etape 23: convertir le auth en bloc

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesbycj/constants/routes.dart';
 import 'package:notesbycj/services/auth/auth_exceptions.dart';
-import 'package:notesbycj/services/auth/auth_service.dart';
+import 'package:notesbycj/services/auth/bloc/auth_bloc.dart';
+import 'package:notesbycj/services/auth/bloc/auth_event.dart';
 import 'package:notesbycj/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -48,27 +50,7 @@ class _LoginViewState extends State<LoginView> {
               final password = _password.text;
 
               try {
-                await AuthService.firebase().logIn(
-                  email: email,
-                  password: password,
-                );
-
-                final user = AuthService.firebase().currentUser;
-
-                if (user?.isEmailVerified ?? false) {
-                  if (context.mounted) {
-                    Navigator.of(
-                      context,
-                    ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
-                  }
-                } else {
-                  if (context.mounted) {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      verifyEmailRoute,
-                      (route) => false,
-                    );
-                  }
-                }
+                context.read<AuthBloc>().add(AuthEventLogin(email, password));
               } on UserNotFoundAuthException {
                 if (context.mounted) {
                   await showErrorDialog(context, 'Utilisateur introuvable.');
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext;
 import 'package:notesbycj/constants/routes.dart';
 import 'package:notesbycj/enums/menu_action.dart';
+import 'package:notesbycj/services/auth/bloc/auth_bloc.dart';
+import 'package:notesbycj/services/auth/bloc/auth_event.dart';
 import 'package:notesbycj/services/cloud/cloud_note.dart';
 import 'package:notesbycj/services/auth/auth_service.dart';
 import 'package:notesbycj/views/notes/notes_list_view.dart';
@@ -44,11 +47,8 @@ class _NotesViewState extends State<NotesView> {
                 case MenuAction.logout:
                   final shouldLogout = await showLogoutDialog(context);
                   if (shouldLogout) {
-                    await AuthService.firebase().logOut();
                     if (context.mounted) {
-                      Navigator.of(
-                        context,
-                      ).pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                      context.read<AuthBloc>().add(const AuthEventLogout());
                     }
                   }
                   break;
