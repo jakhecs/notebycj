09c90ee82696f36a171babf0556573dbf817a7b4
jakhecs <jakhecs@gmail.com>
2025-06-05 13:51:49 +0000

    Etape28: app icon & app name

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -6,9 +6,9 @@ import 'package:notesbycj/services/auth/bloc/auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc(AuthProvider provider)
     : super(const AuthStateUninitialized(isLoading: true)) {
-    on<AuthEventShouldRegister>((event, emit) {
-      emit(const AuthStateRegistering(exception: null, isLoading: false));
-    });
+    // on<AuthEventShouldRegister>((event, emit) {
+    //   emit(const AuthStateRegistering(exception: null, isLoading: false));
+    // });
     // Mot de passe oublié
     on<AuthEventForgotPassword>((event, emit) async {
       emit(
